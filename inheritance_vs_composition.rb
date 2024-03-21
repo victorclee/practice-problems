5a8feba58d4bb4cc69672e215ebb439cb4a47abe
victorclee <victorchristopherlee@gmail.com>
2024-03-20 19:58:47 -0500

    Updated notes and ran standardrb

--- inheritance_vs_composition.rb
+++ inheritance_vs_composition.rb
@@ -1,7 +1,7 @@
 # Putting composition into practice
 # References
 # https://medium.com/@MinimalGhost/inheritance-versus-composition-in-ruby-4ff52beb5e86
-# https://medium.com/aviabird/ruby-composition-over-inheritance-3ff786ad9e5d This post might be questionable
+# https://medium.com/aviabird/ruby-composition-over-inheritance-3ff786ad9e5d This post's title might be questionable. It's talking about mixins
 
 # Inheritance
 class VaderLines
@@ -41,7 +41,6 @@ vader = Character.new(VaderLines.new)
 luke.i_am_your_father
 vader.dark_side
 
-
 # father = Vader.new
 # son = Luke.new
 
@@ -55,15 +54,13 @@ vader.dark_side
 # A technique by which classes may achieve polymorphic behavior and code reuse by containing other classes that implement the desired functionality instead of through inheritance.
 # Ruby uses Modules and a concept called mixins. A Module is essentially a named, organized collection of methods. Mixins are the way in which we require these Modules into our Classes as needed. The methods of a Module can be Class methods or Instance methods, depending on how you add the mixin to the Class (extend and include, respectively).
 
-
-
-class TIE_Fighter_weapons
+class TieFighterWeapons
   def laser_cannons
     puts "TIE Fighter has GREEN laser cannons"
   end
 end
 
-class X_Wing_weapons
+class XWingWeapons
   def laser_cannons
     puts "X-Wing Fighter has RED laser cannons"
   end
@@ -79,13 +76,9 @@ class Spaceship
   end
 end
 
-red_five = Spaceship.new(X_Wing_weapons.new)
-mithel = Spaceship.new(TIE_Fighter_weapons.new)
-
+red_five = Spaceship.new(XWingWeapons.new)
+mithel = Spaceship.new(TieFighterWeapons.new)
 
 red_five.laser_cannons
 
-
-
 mithel.laser_cannons
-
